b08fa109a2d8f1686feff5b8fe2b12a3fe446610
mpoornasaikumar <50005388+mpoornasaikumar@users.noreply.github.com>
2019-05-15 00:25:06 +1000

    Add files via upload

---
+++ lead_process.php
@@ -0,0 +1,65 @@
+<?php
+//must appear BEFORE the <html> tag
+session_start();
+include_once('config.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Lead Process Finished</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="shortcut icon" type="image/x-icon" href="images/logo.png" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="../css/style.css">
+  <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Permanent+Marker" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Caveat" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="https://fonts.googleapis.com/css?family=Mukta+Malar" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="css/member.css">
+   <script src="js/nav.js"></script>
+    <script src="js/read_more.js"></script>
+    <script src="js/member.js"></script>
+</head>
+<body onLoad="run_first()">
+<?php include("include/banner.inc") ?>
+<?php include("include/nav.inc") ?>
+
+<?php
+if(isset($_POST['name'], $_POST['email'], $_POST['number'])) {
+    //make the database connection
+    $conn  = db_connect();
+    $name = $conn -> real_escape_string($_POST['name']);
+    $email = $conn -> real_escape_string($_POST['email']);
+    $number = $conn -> real_escape_string($_POST['number']);
+    //create an insert query
+    $sql = "insert into leads (name, email, number) 
+			VALUES ('$name', '$email', '$number')";
+    //execute the query
+    if($conn -> query($sql))
+    {
+        echo "<div class=\"container\">";    
+        echo "<p>Lead Inserted<p>";
+        echo "<p><a href=\"lead.php\">Add a new lead</a></p>";
+        echo "</div>";
+      
+    }
+    $conn -> close();
+}
+else {
+    die("Input error");
+}
+?>
+
+<?php include("include/footer.inc") ?>
+</body>
+</html>
+
+
+
+
