3b21ac0833c4b3ece79e529dbf3fa1974f9ebce2
mpoornasaikumar <50005388+mpoornasaikumar@users.noreply.github.com>
2019-06-04 23:00:04 +1000

    Update lead_process.php

--- lead_process.php
+++ lead_process.php
@@ -7,39 +7,27 @@ include_once('config.php');
 <html lang="en">
 <head>
     <title>Lead Process Finished</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="shortcut icon" type="image/x-icon" href="images/logo.png" />
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" type="text/css" href="../css/style.css">
-  <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=Permanent+Marker" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=Caveat" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link href="https://fonts.googleapis.com/css?family=Mukta+Malar" rel="stylesheet">
-  <link rel="stylesheet" type="text/css" href="css/style.css">
-  <link rel="stylesheet" type="text/css" href="css/member.css">
-   <script src="js/nav.js"></script>
-    <script src="js/read_more.js"></script>
-    <script src="js/member.js"></script>
+    <?php include("include/header.inc") ?>
 </head>
 <body onLoad="run_first()">
 <?php include("include/banner.inc") ?>
 <?php include("include/nav.inc") ?>
 
 <?php
-if(isset($_POST['name'], $_POST['email'], $_POST['number'])) {
+if(isset($_POST['lead_id'], $_POST['name'], $_POST['source'], $_POST['number'], $_POST['email'], $_POST['status'])) {
     //make the database connection
     $conn  = db_connect();
+    $leadid = $conn -> real_escape_string($_POST['lead_id']);
     $name = $conn -> real_escape_string($_POST['name']);
+    $source = $conn -> real_escape_string($_POST['source']);
+    $contact = $conn -> real_escape_string($_POST['number']);
     $email = $conn -> real_escape_string($_POST['email']);
-    $number = $conn -> real_escape_string($_POST['number']);
-    //create an insert query
-    $sql = "insert into leads (name, email, number) 
-			VALUES ('$name', '$email', '$number')";
+    $status = $conn -> real_escape_string($_POST['status']);
+    //create an insert query 
+   // $sql = "INSERT into leads (ID, Name, Contact, EmailID) 
+    //VALUES ('$leadid', '$name', '$contact', '$email')";
+    $sql = "INSERT into leads (lead_id, Name, Source, Contact, EmailID, Status) 
+    VALUES ('$leadid', '$name', '$source', '$contact', '$email', '$status')";
     //execute the query
     if($conn -> query($sql))
     {
@@ -48,6 +36,10 @@ if(isset($_POST['name'], $_POST['email'], $_POST['number'])) {
         echo "<p><a href=\"lead.php\">Add a new lead</a></p>";
         echo "</div>";
       
+    }
+
+    else{
+        echo "Wrong Inputs";
     }
     $conn -> close();
 }
@@ -59,7 +51,3 @@ else {
 <?php include("include/footer.inc") ?>
 </body>
 </html>
-
-
-
-
