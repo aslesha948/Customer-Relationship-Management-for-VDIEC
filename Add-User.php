a86d090b029118da42ebe92758da240481f1dbb0
aslesha948 <38353215+aslesha948@users.noreply.github.com>
2019-06-05 12:03:47 +1000

    Update Add-User.php

--- Add-User.php
+++ Add-User.php
@@ -25,55 +25,65 @@ if(isset($_SESSION['valid_user']))
 
   
 <form action="member_process.php" method="post">
-         <div class="bg-light mt-3 px-2 member_frm" style="border-radius: 5px; border: solid maroon">
+       <!--  <div class="bg-light mt-3 px-2 member_frm" style="border-radius: 5px; border: solid maroon"> -->
+        <div class="row">
+         <div class="col-md-10">
           <h1>SIGN UP</h1>
-            <h2>Please enter your profile</h2>
+            <h2>Please enter Employee profile</h2>
 
             <div class="row">
-              <div class="col">
-                  <label for="user_id">* Employee ID:</label>
-                    <input type="number" id="user_id" name="user_id" size="10" maxlength="10" placeholder="YYYYXXX" required />
+              <div class="col-md-2" >
+                  <label for="user_id">*Employee ID:</label> </div>
+                  <div class="col-md-8">
+                    <input type="number" id="user_id" name="user_id" size="10" maxlength="10" placeholder="YYYYXXX" class="form-control" required />
+                </div>
+            </div>
+
+            <div class="row">
+              <div class="col-md-2">
+                  <label for="name">* Name:</label></div>
+                  <div class="col-md-8">
+                    <input type="text" id="name" name="name" size="30" maxlength="50" class="form-control"  required />
                 </div>
             </div>
             <div class="row">
-              <div class="col">
-                  <label for="name">* Name:</label>
-                    <input type="text" id="name" name="name" size="30" maxlength="50" required />
+               <div class="col-md-2">
+                  <label for="email">* Email:</label></div>
+                   <div class="col-md-8">
+                  <input type="email" id="email" name="email" size="30" maxlength="50" class="form-control" required />
                 </div>
             </div>
             <div class="row">
-              <div class="col">
-                  <label for="email">* Email:</label>
-                  <input type="email" id="email" name="email" size="30" maxlength="50" required />
-                </div>
-            </div>
-            <div class="row">
-              <div class="col">
-                  <label for="password">* Password:</label>
-                    <input type="password" id="password" name="password" size="30" maxlength="50" required />
+               <div class="col-md-2">
+                  <label for="password">* Password:</label></div>
+                   <div class="col-md-8">
+                    <input type="password" id="password" name="password" size="30" maxlength="50" class="form-control" required />
                     <span id="pwd_msg" class="error_msg"></span>
                 </div>
             </div>
             <div class="row">
-              <div class="col">
-                  <label for="rePassword">* Retype Password:</label>
-                    <input type="password" id="rePassword" size="30" maxlength="50"
+               <div class="col-md-2">
+                  <label for="rePassword">* Retype Password:</label></div>
+                   <div class="col-md-8">
+                    <input type="password" id="rePassword" size="30" maxlength="50" class="form-control"
                                onChange="checkRePassword(document)" />
                 </div>
             </div>
             <div class="row">
-              <div class="col">
-                  <label for="number">* Mobile Number:</label>
-                    <input type="text" id="number" name="number" size="10" maxlength="10" placeholder="10 digits"
+              <div class="col-md-2">
+                  <label for="number">* Mobile Number:</label></div>
+                   <div class="col-md-8">
+                    <input type="text" id="number" name="number" size="10" maxlength="10" placeholder="10 digits" class="form-control"
                                onChange="checkphoneNumber(document)" required />
                     <span class="error_msg" id="num_msg"></span>
                 </div>
             </div>
 
             <div class="row">
-              <div class="col">
-                  <label for="address">* Address:</label>
-                  <input type="address" id="address" name="address" size="30" maxlength="50" required />
+               <div class="col-md-2">
+                  <label for="address">* Address:</label></div>
+                   <div class="col-md-8">
+                  <input type="text" id="address" name="address" size="30" maxlength="50" class="form-control" />
                 </div>
             </div>
 
@@ -84,7 +94,9 @@ if(isset($_SESSION['valid_user']))
                     <input type="reset" id="reset" value="Clear Form" onClick="reset_frm(document)" />
                 </div>
             </div>
+
         </div>
+      </div>
         </form>
       
 
